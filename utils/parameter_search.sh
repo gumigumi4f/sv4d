291f5f57c67e3421391b1b08004c6a8172ffa89f
gumigumi7 <gumizawa@jnlp.org>
2018-10-24 11:52:02 +0900

    Fix param

--- utils/parameter_search.sh
+++ utils/parameter_search.sh
@@ -1,6 +1,6 @@
-for beta_dict in `seq 2.5 4.5`
+for beta_dict in `seq 0.25 0.1 0.45`
 do
-	for beta_reward in `seq 2.5 4.5`
+	for beta_reward in `seq 0.25 0.1 0.45`
 	do
 		mkdir "../models/bd${beta_dict}_br${beta_reward}"
 		../bin/sv4d training -training_corpus ../corpus/WestburyLab.Wikipedia.ProcessedCorpus.txt -synset_data_file ../corpus/sense.txt -model_dir ../models/bd${beta_dict}_br${beta_reward} -thread_num 32 -epochs 10 -min_beta_dict ${beta_dict} -min_beta_reward ${beta_reward}
